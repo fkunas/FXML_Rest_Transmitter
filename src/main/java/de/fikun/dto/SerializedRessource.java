a036cf6f8f568614d787773c5bcf2ddf1872c252
triloX <finn.kunas@freenet.de>
2017-06-02 21:45:09 +0200

    Added Source to ignore

--- src/main/java/de/fikun/dto/SerializedRessource.java
+++ src/main/java/de/fikun/dto/SerializedRessource.java
@@ -1,5 +1,11 @@
 package de.fikun.dto;
 
+import java.io.File;
+
 public class SerializedRessource {
+	static File currentDir = new File(".");
+	public static void main(String[] args){
+		System.out.println(currentDir.getAbsolutePath());
+	}
 
 }
